cb33fcba2f72b8c41942aa9508aeea37c1ab0bc7
Vaishnavi Karanjkar <karanjkarv4@gmail.com>
2023-08-15 01:39:21 +0530

    initial commit

---
+++ TempSensor1/sketch_apr23a/sketch_apr23a.ino
@@ -0,0 +1,67 @@
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <DHT.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+#define BLYNK_TEMPLATE_ID "TMPL3MItYuAMl"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "86ULHSj3XYQ2QzZj1dBuIp_blJoshmHb"
+
+//char auth[] = "37C-Go9VnnkcLjQdTEOFkXlVnpO0NO_x";
+char auth[] = "86ULHSj3XYQ2QzZj1dBuIp_blJoshmHb"; //Enter the Auth code which was send by Blink
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "VAISHNAVI HOME 4G";  //Enter your WIFI Name
+char pass[] = "rishikesh2611";  //Enter your WIFI Password
+
+#define DHTPIN 2          // Digital pin 4
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+}
+
+void setup()
+{
+  Serial.begin(9600); // See the connection status in Serial Monitor
+  Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
+}
+
+
